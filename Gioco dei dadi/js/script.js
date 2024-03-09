c9afc73e6399daf23f51c21b18f37892b46ac454
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-09 15:18:30 +0100

    feat: add element html and print result

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -2,22 +2,45 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-            let numCasual = [1, 2, 3, 4, 5, 6];
-            numCasual = Math.floor(Math.random() * numCasual.length);
-            console.log (numCasual);
+const generateBtn = document.querySelector('#playGames');
+generateBtn.addEventListener('click', function() {
+    let numCasual = [1, 2, 3, 4, 5, 6];
+    numCasual = Math.floor(Math.random() * numCasual.length);
+    let txtUser = ' è il tuo numero';
+    let printResultuman = numCasual + txtUser;
+    console.log (printResultuman)
+    
+    let pointUman = document.querySelector('#punteggio');
+    pointUman.innerHTML = printResultuman;
 
-            let pcNum = [1, 2, 3, 4, 5, 6];
-            pcNum = Math.floor(Math.random() * pcNum.length);
-            console.log (pcNum);
 
-            let risultaoLancio;
+    let pcNum = [1, 2, 3, 4, 5, 6];
+    pcNum = Math.floor(Math.random() * pcNum.length);
+    let txtPc = ' è il numero del computer';
+    let printResultpc = pcNum + txtPc;
+    console.log (printResultpc);
 
-            if (numCasual === pcNum) {
-                risultaoLancio = 'Hai pareggiato';
-            } else if (numCasual > pcNum) {
-                risultaoLancio = 'Hai vinto';
-            } else {
-                risultaoLancio = 'Hai perso';
-            }
+    let pointPc = document.querySelector('#pointInpc');
+    pointPc.innerHTML = printResultpc;
 
-            console.log (risultaoLancio);
+    let risultaoLancio;
+
+    if (numCasual === pcNum) {
+        risultaoLancio = 'Hai pareggiato';
+    } else if (numCasual > pcNum) {
+        risultaoLancio = 'Hai vinto';
+    } else {
+        risultaoLancio = 'Hai perso';
+    }
+
+    console.log (risultaoLancio);
+
+    // seleziono tramite una variabile l'elemento html dove voglio stampare il risultato
+    // successivamente gli riscrivo la variabile con inner ed il contenuto che voglio mostrare
+
+    let risultatoFinale = document.querySelector('#risultatoLa');
+    risultatoFinale.innerHTML = risultaoLancio;
+
+});
+
+   
